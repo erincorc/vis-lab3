3b375fd78bb0f31f09deae1be780bf93a937369c
Erin Corcoran <corcorec@bc.edu>
2020-09-27 21:50:23 -0400

    filtering, svg element to be added next

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //load data initially
+let content;
+
 d3.csv('cities.csv').then(data => {
     console.log('cities ', data);
   }) 
@@ -14,4 +16,24 @@ d3.csv('cities.csv').then(data => {
     }
   }).then(data=>{
       console.log('cities', data);
+      content=data;
   })
+
+let euro;
+d3.csv('cities.csv', d=>{
+  euro = content.filter(c => c.eu == true); //filter out any non-EU countries
+}).then(function(d) {
+  console.log(euro);
+})
+
+d3.select('.city-count').text(28);
+const width = 700;
+const height = 550;
+const svg = d3.select('.population-plot')
+		.append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .selectAll()
+
+
+
