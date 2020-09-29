1db30be90223053476df627eb750551e2685bd51
Erin Corcoran <corcorec@bc.edu>
2020-09-28 22:23:45 -0400

    got circles to appear

--- script.js
+++ script.js
@@ -19,21 +19,37 @@ d3.csv('cities.csv').then(data => {
       content=data;
   })
 
+
 let euro;
-d3.csv('cities.csv', d=>{
-  euro = content.filter(c => c.eu == true); //filter out any non-EU countries
+d3.csv('cities.csv', function(error, d){
+    euro = content.filter(c => c.eu == true); //filter out any non-EU countries
+    console.log(euro);
 }).then(function(d) {
-  console.log(euro);
+  d3.select('.city-count').text('Number of cities: ').text(28);
+  const width = 700;
+  const height = 550;
+  const svg = d3.select('.population-plot')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+let circ = svg.selectAll("population-plot")
+      .data(euro)
+      .enter()
+      .append("circle")
+      .attr('cx', euro=>{
+        return euro.x;
+      })
+      .attr('cy', euro=> {
+        return euro.y;
+      })
+      .attr('r', euro => {
+        if (euro.population < 1000000) return 4;
+        else return 8;
+      });
+
 })
 
-d3.select('.city-count').text(28);
-const width = 700;
-const height = 550;
-const svg = d3.select('.population-plot')
-		.append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .selectAll()
+
 
 
 
