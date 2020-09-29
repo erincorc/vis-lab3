4cd2f9acc3eafd07fe9d565eb408e1c2c753aee2
Erin Corcoran <corcorec@bc.edu>
2020-09-28 23:02:28 -0400

    working on the labels for circles

--- script.js
+++ script.js
@@ -21,18 +21,22 @@ d3.csv('cities.csv').then(data => {
 
 
 let euro;
-d3.csv('cities.csv', function(error, d){
+let circ;
+let bigCities;
+let text;
+
+d3.csv('cities.csv').then(function(data) {
     euro = content.filter(c => c.eu == true); //filter out any non-EU countries
     console.log(euro);
 }).then(function(d) {
-  d3.select('.city-count').text('Number of cities: ').text(28);
+  d3.select('.city-count').text('Number of European cities: ' + 28);
   const width = 700;
   const height = 550;
   const svg = d3.select('.population-plot')
       .append('svg')
       .attr('width', width)
       .attr('height', height)
-let circ = svg.selectAll("population-plot")
+circ = svg.selectAll("population-plot")
       .data(euro)
       .enter()
       .append("circle")
@@ -46,8 +50,20 @@ let circ = svg.selectAll("population-plot")
         if (euro.population < 1000000) return 4;
         else return 8;
       });
-
+bigCities = euro.filter(e => (e.eu == true && e.population >1000000));
+text = circ.select("text")
+      .data(bigCities)
+      .enter()
+      .append("text")
+      .attr('x', bigCities => bigCities.x)
+      .attr('y', bigCities => bigCities.y)
+      .text(bigCities.city)
+      .attr('font-size', "11px")
+      .attr('text-anchor', 'middle');
+      
 })
+
+
 
 
 
